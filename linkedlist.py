5ff1166ac6bbf540100a91e65b047391be83d4fe
Joon Kim <joonkim@eduroam-163-28.dyn.carleton.edu>
2017-03-20 22:04:35 -0500

    change linkedList

--- linkedlist.py
+++ linkedlist.py
@@ -1,21 +1,26 @@
 class LinkedListNode(object):
 
-    """Docstring for LinkedListNode. """
+	def __init__(self, val):
+		self.val = val
+		self.next = None
 
-    def __init__(self, val):
-        self.val = val
-        self.next = None
+	def add(self, node):
+    	current = self
+		while (current.next != None):
+    	current = current.next
+	current.next = node
 
-    def add(self, node):
-        current = self
-        while (current.next != None):
-            current = current.next
-        current.next = node 
+	def printList(self):
+	    current = self
+	while current.next != None:
+	    print(current)
+	print(current.val)
+	current = current.next
 
 jimmy = LinkedListNode(4)
 david = LinkedListNode(2)
+simon = LinkedListNode(3)
 
-jimmy.add(david)
-print(jimmy.next.val)
-
-
+jimmy.next = david
+jimmy.add(simon)
+jimmy.printList()
